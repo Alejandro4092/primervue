4854e86ef6114f1c316e71aa426387e1d924e5e8
Alejandro4092 <acobomarcos@gmail.com>
2025-10-27 09:30:25 +0100

    parImpar

--- src/components/ParImpar.vue
+++ src/components/ParImpar.vue
@@ -3,6 +3,11 @@
     <h1>
         Par Impar
         <input type="text" v-model="numero">
+        <button @click="GenerarNumero">Numero aleatorio</button>
+        <h3 v-if="numero%2==0">Par</h3>
+        <h3 v-else-if="numero%2!==0">Impar</h3>
+        
+
     </h1>
   </div>
 </template>
@@ -14,6 +19,11 @@ export default {
         return{
         numero:0
         }
+    },
+    methods:{
+      GenerarNumero(){
+        this.numero=Math.floor(Math.random()*100);
+      }
     }
 
 }
